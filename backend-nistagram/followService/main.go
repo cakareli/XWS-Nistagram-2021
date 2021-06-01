6316abfa727e41a8dc2dc486647dab08e2bd5688
denisfruza <denisfruza98@hotmail.com>
2021-06-01 19:15:47 +0200

    Initialized docker and nginx files

--- backend-nistagram/followService/main.go
+++ backend-nistagram/followService/main.go
@@ -1,1 +1,19 @@
-package followService
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Hello from FOLLOW SERVICE!")
+	})
+
+
+	fmt.Printf("Starting server at port 8081\n")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ backend-nistagram/mediaContentService/go.mod
@@ -0,0 +1,3 @@
+module accelerate.com/main
+
+go 1.14
