b11298b747436a38a333dab5894404ef6159a601
cakareli <strahinjacvijanovic@gmail.com>
2021-06-02 16:46:35 +0200

    AuthenticationService model modified

--- backend-nistagram/authenticationService/main.go
+++ backend-nistagram/authenticationService/main.go
@@ -8,7 +8,9 @@ import (
 )
 
 func main() {
-	var aa model.
+
+	u := model.user
+
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "Hello from AUTHENTICATION SERVICE!")
 	})
--- backend-nistagram/authenticationService/model/user.go
+++ backend-nistagram/authenticationService/model/user.go
@@ -4,3 +4,8 @@ package model
 type User struct{
 	Name string
 }
+
+func newUser() *User{
+	u := User{Name: "brt"}
+	return &u
+}
