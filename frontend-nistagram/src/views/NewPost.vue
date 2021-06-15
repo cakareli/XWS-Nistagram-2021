5dc9108d6317f9ab5450749a6966e978277b5966
denisfruza <denisfruza98@hotmail.com>
2021-06-15 22:40:33 +0200

    Implemented adding new post in frontend

--- frontend-nistagram/src/views/NewPost.vue
+++ frontend-nistagram/src/views/NewPost.vue
@@ -1,14 +1,29 @@
 <template>
     <v-container>
+        <v-snackbar v-model="snackbar" top timeout="3500">
+            <span>{{snackbarText}}</span>
+        </v-snackbar>
+        <v-flex>
         <v-row>
-            <v-form>
+        <v-col>
+            <v-form v-model="form.isFormValid">
                 <v-btn raised class="grey lighten-3" @click="uploadFile">Upload Image</v-btn>
                 <input type="file" style="display: none" ref="fileInput" accept="image/*" @change="onFilePicked"/>
+                <v-text-field label ="Description" v-model="form.description" :rules="rules.description"></v-text-field>
+                <v-text-field label ="Location" v-model="form.location" :rules="rules.location"></v-text-field>
+                <v-row>
+                    <v-text-field label ="Tags" v-model="tag" @click="addTag"></v-text-field>
+                    <v-btn class="grey lighten-3 ma-3" @click="addTag">Add Tag</v-btn>
+                </v-row>
+                <v-textarea outlined label ="All tags" v-model="form.tags" rows="2" no-resize readonly></v-textarea>
+                <v-btn color="grey lighten-3" allign-right :disabled="!form.isFormValid" @click="submit">Publish Post</v-btn>
             </v-form>
-        </v-row>
-        <v-row>
+        </v-col>
+        <v-col>
             <img :src="imageUrl" height="500" width="400"/>
+        </v-col>
         </v-row>
+        </v-flex>
     </v-container>
 </template>
 
@@ -16,6 +31,8 @@
 
 import firebase from 'firebase/app'
 import 'firebase/firebase-storage'
+import { getUsername } from '../security/token.js'
+import axios from 'axios'
 
 export default {
     name: 'AddPostComment',
@@ -27,7 +44,26 @@ export default {
             imageUrl: "",
             image: null,
             uploadValue: 0,
-            firebaseURL: "",
+            firebaseURL: [],
+            tag : "",
+            form:{
+                name: "",
+                tags: [],
+                location: "",
+                isFormValid: false,
+            },
+            rules:{
+                descriptionRules: [
+                    description => !!description || 'Description is required!',
+                    description => (description && description.length <= 100) || 'Description must contain less than 100 characters!',
+                ],
+                locationRules: [
+                    description => !!description || 'Description is required!',
+                    description => (description && description.length <= 30) || 'Description must contain less than 100 characters!',
+                ],
+            },
+            snackbar: false,
+            snackbarText: ""
         }
     },
     mounted() {
@@ -58,11 +94,42 @@ export default {
                 }, error=>{console.log(error.message)},
                 ()=>{this.uploadValue=100;
                 storageRef.snapshot.ref.getDownloadURL().then((url)=>{
-                this.firebaseURL =url;
+                this.firebaseURL.push(url)
                 console.log(this.firebaseURL)
                 });
             });
-            
+        },
+        addTag(){
+            if(this.tag != "" && !this.form.tags.includes(this.tag))
+                this.form.tags.push(this.tag);
+            this.tag = "";
+        },
+        submit() {
+            if(this.firebaseURL == ""){
+                alert("Image is still not uploaded!")
+            }else {
+                let postUploadDTO = {
+                description : this.form.description,
+                tags : this.form.tags,
+                mediaPaths : this.firebaseURL,
+                uploadDate: new Date(),
+                location: this.form.location,
+                username: getUsername(),
+                }
+                axios.post('http://localhost:8081/api/media-content/create-new-post/',
+                    postUploadDTO
+                ).then(response => {
+                    console.log(response)
+                }).catch(error => {
+                    if(error.response.status === 500){
+                        this.snackbarText = "Internal server error occurred!";
+                        this.snackbar = true;
+                    }else if(error.response.status === 400){
+                        this.snackbarText = "Bad request, try again!";
+                        this.snackbar = true;
+                    }
+                })
+            }
         }
     }
 }
