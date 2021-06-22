e862b8e1fe01bd21fb6e1d043117b2df1e2ad04b
mihajlo-jaric <mihajlo.jaric@gmail.com>
2021-06-22 14:45:17 +0200

    bugs fixed

--- backend-nistagram/userService/repository/regularUserRepository.go
+++ backend-nistagram/userService/repository/regularUserRepository.go
@@ -128,7 +128,7 @@ func (repository *RegularUserRepository) FindUserByUsername(username string) (*m
 func (repository *RegularUserRepository) GetAllPublicRegularUsers() ([]bson.D, error){
 
 	usersCollection := repository.Database.Collection("regularUsers")
-	filterCursor, err := usersCollection.Find(context.TODO(), bson.M{"profilePrivacy.privacyType": 0})
+	filterCursor, err := usersCollection.Find(context.TODO(), bson.M{"privacyType": 0})
 	if err != nil {
 		log.Fatal(err)
 	}
